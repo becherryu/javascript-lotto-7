6990dfad3114611e85c88e68df91447469bf5fdd
becherryu <81617058+becherryu@users.noreply.github.com>
2024-11-04 23:55:35 +0900

    feat(Lotto.js): 예외 출력하기

--- src/Lotto.js
+++ src/Lotto.js
@@ -10,9 +10,30 @@ class Lotto {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
+
+    const duplicates = numbers.filter(
+      (item, index) => numbers.indexOf(item) !== index
+    );
+    if (duplicates.length > 0) {
+      throw new Error(
+        `[ERROR] 중복된 당첨 번호가 있습니다. : ${duplicates.join(", ")}`
+      );
+    }
+
+    if (
+      numbers.some((num) => isNaN(num) || !Number.isInteger(num) || num === "")
+    ) {
+      throw new Error("[ERROR] 로또 번호는 숫자만 입력할 수 있습니다.");
+    }
+
+    if (numbers.some((num) => num < 1 || num > 45)) {
+      throw new Error("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+    }
   }
 
-  // TODO: 추가 기능 구현
+  getNumbers() {
+    return this.#numbers;
+  }
 }
 
 export default Lotto;
