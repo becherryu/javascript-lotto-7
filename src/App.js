d3a0b2c137ef1acae061e4be4665e19177a559cf
becherryu <81617058+becherryu@users.noreply.github.com>
2024-11-04 22:52:04 +0900

    feat(App.js): 1. 사용자 입력 받기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
+import * as MissionUtils from "@woowacourse/mission-utils";
+import Lotto from "./Lotto.js";
+
 class App {
   async run() {}
+
+  // 1. 로또 구입 금액 입력받기
+  async getLottoBuyAmount() {
+    const AMOUNT = await MissionUtils.Console.readLineAsync(
+      "구입금액을 입력해 주세요.\n"
+    );
+    const INT_AMOUNT = parseInt(AMOUNT, 10);
+
+    if (
+      INT_AMOUNT === "" ||
+      isNaN(INT_AMOUNT) ||
+      INT_AMOUNT < 1000 ||
+      INT_AMOUNT % 1000 !== 0
+    ) {
+      throw new Error("[ERROR] 구입 금액은 1,000원 단위의 숫자여야 합니다.");
+    }
+
+    return INT_AMOUNT;
+  }
 }
 
 export default App;
